4b5aa4de501467b570d1862c59b7d99a95447ff7
Olivier Maridat <olivier.maridat@trialog.com>
2013-04-05 14:07:58 +0200

    Add updater

--- install.php
+++
@@ -1,4 +0,0 @@
-<?php
-// I am here to fake the installation of this "multiple-regex-tool" app.
-// I am only creating a file: THIS_IS_IT_I_AM_INSTALLED, that's all!
-file_put_contents("THIS_IS_IT_I_AM_INSTALLED", "If you can read these lines, that means that this app has been successfully installed.");
--- update.php
+++
@@ -1,4 +0,0 @@
-<?php
-// I am here to fake the update of this "multiple-regex-tool" app.
-// I am only creating a file: THIS_IS_IT_I_AM_UPDATED, that's all!
-file_put_contents("THIS_IS_IT_I_AM_UPDATED", "If you can read these lines, that means that this app has been successfully updated.");
